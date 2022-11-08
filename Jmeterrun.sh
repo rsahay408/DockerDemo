3f59833b5b47bbe559d2986686bad8c0718723ea
rsahay408 <54312741+rsahay408@users.noreply.github.com>
2022-11-08 15:05:35 +0530

    Update Jmeterrun.sh

--- Jmeterrun.sh
+++ Jmeterrun.sh
@@ -2,7 +2,7 @@
 echo "*************Running Jmeterun.sh file**********"
 curtime=$(date +%d%m%Y%H%M%S)
 echo "Current time in epochtime $curtime"
-cd ..
+cd /opt/apache-jmeter-5.5/bin
 sh jmeter.sh -n -t /opt/apache-jmeter-5.5/bin/DockerDemo/SampleTestScript_Google.jmx -l /opt/apache-jmeter-5.5/bin/DockerDemo/Results_${curtime}.jtl -e -o /opt/apache-jmeter-5.5/bin/DockerDemo/HTML_${curtime}
 cd DockerDemo
 #sleep 1m
