a910bc01508d23825cb549154495b09f5a36aad9
rsahay408 <54312741+rsahay408@users.noreply.github.com>
2022-11-08 12:46:03 +0530

    Add files via upload

---
+++ Jmeterrun.sh
@@ -0,0 +1,28 @@
+#!bin/bash
+
+echo "*************Running Jmeterun.sh file**********"
+
+#git config --global http.sslverify false
+#git clone https://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@github.com/svodhulu/Docker.git
+
+
+curtime=$(date +%d%m%Y%H%M%S)
+
+echo "Current time in epochtime $curtime"
+
+cd ..
+sh jmeter.sh -n -t /opt/apache-jmeter-5.5/bin/Docker/SampleTestScript_Google.jmx -l /opt/apache-jmeter-5.5/bin/Docker/Results_${curtime}.jtl -e -o /opt/apache-jmeter-5.5/bin/Docker/HTML_${curtime}
+ 
+cd Docker
+
+sleep 1m
+#Push the code to repo
+git add .
+git config --global user.name "SaiKumar"
+git config --global user.email "saikumar.vodhulu@qentelli.com"
+
+git commit -m "Results"
+git push
+
+
+echo "********Test executed SUCCESSFULLY*******"
