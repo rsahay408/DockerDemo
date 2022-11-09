7cfbe8ba63713dbac0a624beada563c5fcd5430c
rsahay408 <54312741+rsahay408@users.noreply.github.com>
2022-11-09 19:42:23 +0530

    Update Jmeterrun.sh

--- Jmeterrun.sh
+++ Jmeterrun.sh
@@ -2,12 +2,12 @@
 echo "*************Running Jmeterun.sh file**********"
 curtime=$(date +%d%m%Y%H%M%S)
 echo "Current time in epochtime $curtime"
-#cd /opt/apache-jmeter-5.5/bin
-alias Jmeterrun="cd /opt/apache-jmeter-5.5/bin"
+cd ..
+
 sh jmeter.sh -n -t /opt/apache-jmeter-5.5/bin/DockerDemo/SampleTestScript_Google.jmx -l /opt/apache-jmeter-5.5/bin/DockerDemo/Results_${curtime}.jtl -e -o /opt/apache-jmeter-5.5/bin/DockerDemo/HTML_${curtime}
-#cd DockerDemo
-alias Jmeterrun="cd DockerDemo"
-#sleep 1m
+cd DockerDemo
+
+sleep 1m
 #Push the code to repo
 git add .
 git config --global user.name "rsahay408"
