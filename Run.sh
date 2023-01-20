ff78151ab4d0435080a20802c1d394f884827d88
rsahay408 <54312741+rsahay408@users.noreply.github.com>
2023-01-20 15:57:37 +0530

    Update Run.sh

--- Run.sh
+++ Run.sh
@@ -6,8 +6,8 @@ echo "******************* ${ROOT_DIR}********************"
 
 echo "**********************Building An IMAGE*******************"
 
-#docker build -t jmeterupdated Container3
-winpty docker build --no-cache -t jmeterupdated . 
+docker build -t jmeterupdated .
+#winpty docker build --no-cache -t jmeterupdated . 
 echo "**********************Running a CONTAINER*****************"
 
 #docker run -v "${ROOT_DIR}:/opt/apache-jmeter-5.5/bin/code" -d jmeterupdated/latest
