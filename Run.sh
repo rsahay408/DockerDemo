52094b02ad8132f39baca29aac650f93f0f621f9
rsahay408 <rupesh.sahay@qentelli.com>
2023-01-20 12:35:14 +0530

    SH

---
+++ Run.sh
@@ -0,0 +1,21 @@
+#!bin/bash
+
+ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+echo "******************* ${ROOT_DIR}********************"
+
+echo "**********************Building An IMAGE*******************"
+
+#docker build -t jmeterupdated Container3
+winpty docker build --no-cache -t jmeterupdated . 
+echo "**********************Running a CONTAINER*****************"
+
+#docker run -v "${ROOT_DIR}:/opt/apache-jmeter-5.5/bin/code" -d jmeterupdated/latest
+chmod 777 Jmeterrun.sh
+
+docker run -it jmeterupdated
+
+echo "**********************Run Started***********"
+
+echo "CONTAINER RAN SUCCESSFULLY"
+sleep 2m
