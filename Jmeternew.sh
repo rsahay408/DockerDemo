cb2876a5c2a2bc119092ad2b2b50212dd3b564e1
rsahay408 <54312741+rsahay408@users.noreply.github.com>
2023-01-27 20:52:53 +0530

    Update Jmeternew.sh

--- Jmeternew.sh
+++ Jmeternew.sh
@@ -1,21 +1,14 @@
 #!bin/bash
 echo "*************Running Jmeterun.sh file**********"
-
 curtime=$(date +%d%m%Y%H%M%S)
-
 #Results Folder
 mkdir Results_${curtime}
-
 echo "Current time in epochtime $curtime"
-
 #Move to bin path
 cd ..
-
 sh jmeter.sh -n -t /opt/apache-jmeter-5.5/bin/DockerDemo/SampleTestScript_Google.jmx -l /opt/apache-jmeter-5.5/bin/DockerDemo/Results_${curtime}/Results_${curtime}.jtl -e -o /opt/apache-jmeter-5.5/bin/DockerDemo/Results_${curtime}/HTML_${curtime}
- 
 #After Run Move to Repo
 cd DockerDemo
-
 sleep 1m
 #Push the code to repo
 git add .
